4034b048eee1654e3ab463ff4c26f0bd78eea8b6
Hammad Shaikh <55096761+shkhammad@users.noreply.github.com>
2026-09-23 01:14:32 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        
+        int i,res[] = new int[n];
+        res[0]=1;res[1]=2;
+
+        for(i=2;i<n;++i)
+            res[i] = res[i-1] + res[i-2];
+        
+        return res[n-1];
+    }
+}
